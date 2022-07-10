5a4fc808b4ea6f78d50436f7524437a5617b1a04
Phil Roth <mr.phil.roth@gmail.com>
2022-07-10 19:17:08 -0400

    linkedin update. poetry dependencies.

--- publishconf.py
+++ publishconf.py
@@ -7,8 +7,9 @@ from __future__ import unicode_literals
 
 import os
 import sys
+
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://mrphilroth.com'
+SITEURL = "https://mrphilroth.com"
 RELATIVE_URLS = False
